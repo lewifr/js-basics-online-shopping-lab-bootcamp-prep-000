fd531a7aa6f321e285655be7af71efa728e0ef01
Francis Lewis <francis.lewis@me.com>
2019-04-12 16:04:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ function placeOrder(cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   } else {
     cart = []
-    let cartTotal = total()
+    let cartTotal = this.total()
     console.log(cartTotal)
     // return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
   }
-  }
+}
